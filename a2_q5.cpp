fdb16dfa9ed47621f99a81b2d581bc277589e9f8
Dakshita Agrawal <145870184+DaksCodes@users.noreply.github.com>
2023-12-28 20:11:18 +0530

    Create a2_q5.cpp

---
+++ a2_q5.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter an odd number\n";
+    cin>>num;
+    int h=num/2;
+    for(int i=1; i<=h;i++)
+    {
+        cout<<" ";
+        for(int j=1; j<=h-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1; k<=(2*i)-1; k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1; i<=num; i++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+    for(int i=1; i<=h; i++)
+    {
+        for(int j=1; j<=i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=num-2; k>=1; k-- )
+        {
+            cout<<"*";
+            
+        }
+        cout<<endl;
+        num=num-2;
+    }
+    return 0;
+}
