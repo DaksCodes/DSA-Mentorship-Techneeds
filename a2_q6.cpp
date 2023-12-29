c52a8d959783ec597129feb47643ee8da452b759
Dakshita Agrawal <145870184+DaksCodes@users.noreply.github.com>
2023-12-29 12:35:08 +0530

    Create a2_q6.cpp

---
+++ a2_q6.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter an odd number\n";
+    cin>>num;
+    int h=num/2;
+    for(int i=0; i<=h;i++)
+    {
+        for(int j=1; j<=h+1-i;j++)
+        {
+            cout<<"*";
+        }
+        for(int j=1; j<=(2*i)+1;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1; j<=h+1-i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1; i<=h; i++)
+    {
+        for(int j=1; j<=i+1; j++)
+        {
+            cout<<"*";
+        }
+        for(int j=1; j<= 2*(h-i)+1;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1; j<=i+1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
